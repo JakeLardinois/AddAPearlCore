99bb942f894a489037d5050ea257a2c9bfee93e7
JakeLardinois <jake.lardinois@gmail.com>
2016-11-25 17:51:25 -0600

    Removed button from app.component

--- AddAPearl.Web/app/app.component.ts
+++ AddAPearl.Web/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 
 @Component({
     selector: 'pearl-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-    <button md-button>My Angular Material Button!!</button>
+    <pearl-companies></pearl-companies>
     </div>
     `
 })
 export class AppComponent {
-    pageTitle: string = `Welcome to Add-A-Pearl!`;
+    pageTitle: string = `Welcome to Add-A-Pearl!`;    
 }

38360613db01b791feb7f753c5b8847fb8ffa0af
JakeLardinois <jake.lardinois@gmail.com>
2016-12-04 21:48:04 -0600

    Set TSLint to not emit error So compile task would continue
    The compile task would not complete if TSLinting failed.  However, I
    want the files to compile even if the linting fails, and then have the
    linting errors displayed on the screen.

--- AddAPearl.Web/gulpfile.ts
+++ AddAPearl.Web/gulpfile.ts
@@ -26,7 +26,9 @@ gulp.task('tslint', () => {
         .pipe($.tslint({
             formatter: 'verbose'
         }))
-        .pipe($.tslint.report());
+        .pipe($.tslint.report({
+            emitError: false
+        }));
 });
 
 gulp.task("compile", ['tslint'], () => {

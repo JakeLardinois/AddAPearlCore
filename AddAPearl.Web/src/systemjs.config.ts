388701483a0d9733dc4f0df3c30ddec8c0634fb9
JakeLardinois <jake.lardinois@gmail.com>
2016-12-06 19:31:26 -0600

    system.js.config.ts LintFixes

--- AddAPearl.Web/src/systemjs.config.ts
+++ AddAPearl.Web/src/systemjs.config.ts
@@ -1,44 +1,40 @@
 /** Type declaration for ambient System. */
 declare var System: any;
 
-
 /**
  * System configuration for Angular 2
  */
-(function (global) {
+((global) => {
   System.config({
-    paths: {
-      // paths serve as alias
-      'npm:': 'lib/'
-    },
     // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
-      app: 'app',
-
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/material': 'npm:@angular/material/material.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/material': 'npm:@angular/material/material.umd.js',
 
       // other libraries
-      'rxjs': 'npm:rxjs'
+      'rxjs': 'npm:rxjs',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
+        defaultExtension: 'js',
         main: './main.js',
-        defaultExtension: 'js'
       },
       rxjs: {
-        defaultExtension: 'js'
-      }
-    }
+        defaultExtension: 'js',
+      },
+    },
+    paths: {
+      // paths serve as alias
+      'npm:': 'lib/',
+    },
   });
 })(this);

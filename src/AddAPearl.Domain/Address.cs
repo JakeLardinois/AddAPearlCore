4bedcc7cdbb6355d9a74b805ed51c2d34c6c22cf
JakeLardinois <jake.lardinois@gmail.com>
2016-12-09 06:21:03 -0600

    Added Domain Classes

---
+++ src/AddAPearl.Domain/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace AddAPearl.Domain
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        
+        public string AddressLine1 { get; set; }
+        
+        public string AddressLine2 { get; set; }
+        
+        public string AddressLine3 { get; set; }
+        
+        public string City { get; set; }
+        
+        public string State { get; set; }
+        
+        public string ZipCode { get; set; }
+    }
+}
---
+++ src/AddAPearl.Domain/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace AddAPearl.Domain
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        
+        public string CompanyName { get; set; }
+
+        public int? AddressId { get; set; }
+
+        public Address Address { get; set; }
+    }
+}

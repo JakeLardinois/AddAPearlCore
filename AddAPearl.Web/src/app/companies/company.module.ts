386cd011577229c60bcf35e2c46b46db62ba7145
JakeLardinois <jake.lardinois@gmail.com>
2016-12-17 06:22:29 -0600

    company.module lint fixes

--- AddAPearl.Web/src/app/companies/company.module.ts
+++ AddAPearl.Web/src/app/companies/company.module.ts
@@ -1,35 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule} from '@angular/router';
 
-import { CompanyService } from '../shared/services/company.service';
-import { CompanyListComponent }  from './company-list.component';
-import { CompanyFilterPipe } from './company-filter.pipe';
 import { AddressDialog, StarComponent }  from '../shared/index';
-
+import { CompanyService } from '../shared/services/company.service';
+import { CompanyFilterPipe } from './company-filter.pipe';
+import { CompanyListComponent }  from './company-list.component';
 
 @NgModule({
+  declarations: [
+    CompanyListComponent,
+    AddressDialog,
+    CompanyFilterPipe,
+    StarComponent,
+  ],
+  entryComponents: [
+    AddressDialog,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     MaterialModule.forRoot(),
     RouterModule.forChild([
-      { path: 'companies', component: CompanyListComponent }
-    ])
-  ],
-  declarations: [
-    CompanyListComponent,
-    AddressDialog,
-    CompanyFilterPipe,
-    StarComponent
-  ],
-  entryComponents: [
-    AddressDialog
+      { path: 'companies', component: CompanyListComponent },
+    ]),
   ],
   providers: [
-    CompanyService
-  ]
+    CompanyService,
+  ],
 })
+
 export class CompanyModule {}

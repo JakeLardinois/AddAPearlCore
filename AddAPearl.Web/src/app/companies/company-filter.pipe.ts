34642cf84789f311cf9aa913029b65c80f527cbf
JakeLardinois <jake.lardinois@gmail.com>
2016-12-17 05:48:48 -0600

    company-filter lint fixes

--- AddAPearl.Web/src/app/companies/company-filter.pipe.ts
+++ AddAPearl.Web/src/app/companies/company-filter.pipe.ts
@@ -1,13 +1,13 @@
-import { PipeTransform, Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 import { ICompany } from '../shared/models/company';
 
 @Pipe({
-    name: 'companyFilter'
+    name: 'companyFilter',
 })
 export class CompanyFilterPipe implements PipeTransform {
 
-    transform(value: ICompany[], filterBy: string): ICompany[] {
+    public transform(value: ICompany[], filterBy: string): ICompany[] {
         filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
         return filterBy ? value.filter((company: ICompany) =>
             company.companyName.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;

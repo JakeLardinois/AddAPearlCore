e1ac8d1b6b469d1b844c3247dc5cdf2d06d3489f
JakeLardinois <jake.lardinois@gmail.com>
2016-12-24 21:59:26 -0600

    Formatted Snackbar

--- AddAPearl.Web/src/app/shared/address-dialog/addressDialog.component.ts
+++ AddAPearl.Web/src/app/shared/address-dialog/addressDialog.component.ts
@@ -13,16 +13,16 @@ import { AddressService, IAddress } from '../index';
 export class AddressDialog {
   public addressName: string;
   public address: IAddress;
-  constructor(public dialogRef: MdDialogRef<AddressDialog>, public snackBar: MdSnackBar) { }
-  public addressService: AddressService;
+  public constructor(public dialogRef: MdDialogRef<AddressDialog>, public snackBar: MdSnackBar) { }
+  public addressService: AddressService
   public errorMessage: string;
   public updateAddress(): void {
     this.addressService.updateAddress(this.address)
-                .subscribe((address) => this.address = address,
-                           (error) => this.errorMessage = <any> error);
+      .subscribe((address) => this.address = address,
+        (error) => this.errorMessage = <any> error);
       let config = new MdSnackBarConfig();
-        config.duration = 5000;
-        this.snackBar.open('Address Updated: ' + this.address.addressLine1, 'Ok', config);
-        this.dialogRef.close('updated');
+      config.duration = 5000;
+      this.snackBar.open('Address Updated: ' + this.address.addressLine1, 'Ok', config);
+      this.dialogRef.close('updated');
   }
 }

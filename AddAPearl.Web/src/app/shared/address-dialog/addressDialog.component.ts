ff2a4f1746b1192b9676ea87251fa9a13620b17c
JakeLardinois <jake.lardinois@gmail.com>
2016-12-24 22:13:42 -0600

    LintFixes

--- AddAPearl.Web/src/app/shared/address-dialog/addressDialog.component.ts
+++ AddAPearl.Web/src/app/shared/address-dialog/addressDialog.component.ts
@@ -13,16 +13,20 @@ import { AddressService, IAddress } from '../index';
 export class AddressDialog {
   public addressName: string;
   public address: IAddress;
-  public constructor(public dialogRef: MdDialogRef<AddressDialog>, public snackBar: MdSnackBar) { }
-  public addressService: AddressService
+  public addressService: AddressService;
   public errorMessage: string;
+
+  public constructor(public dialogRef: MdDialogRef<AddressDialog>, public snackBar: MdSnackBar) {
+
+  }
+
   public updateAddress(): void {
     this.addressService.updateAddress(this.address)
       .subscribe((address) => this.address = address,
         (error) => this.errorMessage = <any> error);
-      let config = new MdSnackBarConfig();
-      config.duration = 5000;
-      this.snackBar.open('Address Updated: ' + this.address.addressLine1, 'Ok', config);
-      this.dialogRef.close('updated');
+    let config = new MdSnackBarConfig();
+    config.duration = 5000;
+    this.snackBar.open('Address Updated: ' + this.address.addressLine1, 'Ok', config);
+    this.dialogRef.close('updated');
   }
 }

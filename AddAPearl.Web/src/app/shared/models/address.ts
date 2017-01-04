5edbb96a9665b12cc7036a8560914f2655c7170b
JakeLardinois <jake.lardinois@gmail.com>
2017-01-04 05:39:04 -0600

    Added Class to Address for Concrete Initilization
    I added a class to the address definition so that I could initialize a
    concrete instance.

--- AddAPearl.Web/src/app/shared/models/address.ts
+++ AddAPearl.Web/src/app/shared/models/address.ts
@@ -8,3 +8,17 @@ export interface IAddress {
     state: string;
     zipCode: string;
 }
+
+export class Address implements IAddress {
+    constructor(
+        public addressId: number,
+        public addressLine1: string,
+        public addressLine2: string,
+        public addressLine3: string,
+        public city: string,
+        public state: string,
+        public zipCode: string,
+        ) { 
+            //any other stuff to do during construction...
+        }
+}

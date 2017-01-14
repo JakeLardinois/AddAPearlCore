eebe35058306db62bce826efd4c21a6b6779a3e8
JakeLardinois <jake.lardinois@gmail.com>
2017-01-14 15:57:11 -0600

    Moved main Component html to external file
    Also included the css

--- AddAPearl.Web/src/app/app.component.ts
+++ AddAPearl.Web/src/app/app.component.ts
@@ -3,20 +3,10 @@ import {
 } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
 	selector: 'pearl-app',
-	template: `
-    <h1>{{pageTitle}}</h1>
-    <button md-icon-button [md-menu-trigger-for]="menu">
-        <md-icon>more_vert</md-icon>
-    </button>
-    <md-menu #menu="mdMenu">
-        <a md-menu-item [routerLink]="['/welcome']">Home</a>
-        <a md-menu-item [routerLink]="['/companies']">Company List</a>
-    </md-menu>
-    <div class='container'>
-        <router-outlet></router-outlet>
-    </div>
-    `,
+    styleUrls: ['app.component.css'],
+	templateUrl: 'app.component.html',
 })
 export class AppComponent {
 	public pageTitle: string = `Welcome to Add-A-Pearl!`;

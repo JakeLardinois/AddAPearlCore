d27acfc68a30534599d1044cf695cfbe43d9f777
JakeLardinois <jake.lardinois@gmail.com>
2017-01-15 17:30:52 -0600

    Added Address and Company to Customer
    Also fixed lastname to be lowercase

--- AddAPearl.Web/src/app/shared/models/customer.ts
+++ AddAPearl.Web/src/app/shared/models/customer.ts
@@ -1,23 +1,34 @@
+import {
+	IAddress,
+	ICompany,
+} from './index';
+
 /* Defines the customer entity */
 export interface ICustomer {
 	customerId: number;
 	firstName: string;
-	LastName: string;
+	lastName: string;
 	phoneNumber: string;
+	birthday: string;
 	email: string;
 	addressId: number;
+	address: IAddress;
 	companyId: number;
+	company: ICompany,
 }
 
 export class Customer implements ICustomer {
 	constructor(
 		public customerId: number,
 		public firstName: string,
-		public LastName: string,
+		public lastName: string,
 		public phoneNumber: string,
+		public birthday: string,
 		public email: string,
 		public addressId: number,
+		public address: IAddress,
 		public companyId: number,
+		public company: ICompany,
 	) {
 		// Any other stuff to do during construction...
 	}

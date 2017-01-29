4b08dc7c1a40c9a065884a4aabceb123c1f76fa6
JakeLardinois <jake.lardinois@gmail.com>
2017-01-29 09:28:32 -0600

    Implemented Bower
    Added Bower and the necessary gulp tasks to publish the installed bower
    components

--- AddAPearl.Web/gulpfile.ts
+++ AddAPearl.Web/gulpfile.ts
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const del = require('del');
+const wiredep = require('wiredep');
+const runSequence = require('run-sequence');
 const config = require('./gulp.config')();
 const path = require('path');
 const $ = require('gulp-load-plugins')({ lazy: true });
@@ -13,6 +15,76 @@ gulp.task('default', ['help']);
 /* Remove build directory.*/
 gulp.task('clean', (cb:any) => {
     return del(['build'], cb);
+});
+
+gulp.task('processBower', (cb:any) => {
+    runSequence(['bowerScripts', 'bowerCss'], 'indexBower', cb);
+});
+
+//Bower file collection
+gulp.task('bowerScripts', (cb:any) => {
+    var bowerJs = wiredep().js;
+    
+    if(bowerJs && bowerJs.length > 0) { // 'Invalid glob argument' error is thrown by gulp.src if there are no bower scripts
+        return gulp.src(bowerJs)
+            .pipe($.debug({title: 'BowerJavaScript:'}))
+            .pipe($.concat('vendor.min.js'))
+            .pipe($.rev())
+            .pipe($.uglify({
+                mangle: false
+            }))
+            .pipe(gulp.dest('./build/vendor'));
+    } else {
+        Log('No Bower scripts!');
+        cb();
+        return;
+    }
+});
+
+gulp.task('bowerCss', (cb:any) => {
+    var bowerCss = wiredep().css;
+
+    if(bowerCss && bowerCss.length > 0) { // 'Invalid glob argument' error is thrown by gulp.src if there is no bower css
+        return gulp.src(bowerCss)
+            .pipe($.debug({title: 'BowerCss:'}))
+            .pipe($.cssmin())
+            .pipe($.concat('vendor.min.css'))
+            .pipe($.rev())
+            .pipe(gulp.dest('./build/vendor'));
+    } else {
+        Log('No Bower css!');
+        cb();
+        return;
+    }
+});
+
+//inject Bower js & css links into index.html
+gulp.task('indexBower', () => {
+    var bowerJsFiles = ['build/vendor/**/*.js'];
+    var bowerCssFiles = ['build/vendor/**/*.css'];
+
+    return gulp.src('src/index.html')
+
+        .pipe($.inject(
+            gulp.src(bowerJsFiles, {
+                read: false
+            }), {
+                addRootSlash: false,
+                transform: (filePath:any, file:any, i:any, length:any) => {
+                    return '<script src="' + filePath.replace('client/', '') + '"></script>';
+                }
+            }))
+
+        .pipe($.inject(
+            gulp.src(bowerCssFiles, {
+                read: false
+            }), {
+                addRootSlash: false,
+                transform: (filePath:any, file:any, i:any, length:any) => {
+                    return '<link rel="stylesheet" href="' + filePath.replace('client/', '') + '"/>';
+                }
+            }))
+        .pipe(gulp.dest('build'));
 });
 
 /*Compile the SCSS files into the build directory*/
@@ -103,11 +175,11 @@ gulp.task('watch', () => {
         .on('change', changeEvent);
 });
 
-gulp.task('build-prod', ['compile', 'sass', 'resources', 'libs'], () => {
+gulp.task('build-prod', ['compile', 'sass', 'resources', 'libs', 'processBower'], () => {
     Log('Built the production project!');
 });
 
-gulp.task('build-dev', ['compile', 'sass', 'source', 'resources', 'libs'], () => {
+gulp.task('build-dev', ['compile', 'sass', 'source', 'resources', 'libs', 'processBower'], () => {
     Log('Built the development project!');
 });
 

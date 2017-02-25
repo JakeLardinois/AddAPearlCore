ae97d04efb2c6ed41c996674628b12cb969cf0e3
JakeLardinois <jake.lardinois@gmail.com>
2017-02-25 08:02:37 -0600

    Added Hello World Integration Test
    Added a controller that would respond to default api url and then added
    an integration test to check that value

---
+++ src/AddAPearl.API/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using AddAPearl.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AddAPearl.API.Controllers
+{
+    [Route("api/")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogDebug("Hello World!");
+
+            return Ok("Hellow World!");
+        }
+    }
+}

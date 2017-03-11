a459a58863e8f31e5840932a7ff9232ee2661329
JakeLardinois <jake.lardinois@gmail.com>
2017-03-11 07:41:51 -0600

    Added Remaining Controller Integration Tests

---
+++ test/AddAPearl.IntegrationTests/AddAPearlCustomersControllerShould.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using AddAPearl.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AddAPearl.IntegrationTests
+{
+    public class AddAPearlCustomersControllerShould
+    {
+        private readonly IWebHostBuilder _webHostBuilder;
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public AddAPearlCustomersControllerShould()
+        {
+            // Arrange
+            _webHostBuilder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            _server = new TestServer(_webHostBuilder);
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task HaveItems()
+        {
+            var response = await _client.GetAsync("/api/Customers");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var results = JsonConvert.DeserializeObject<IEnumerable<Domain.Customer>>(responseString);
+
+
+            Assert.NotEmpty(results);
+        }
+    }
+}

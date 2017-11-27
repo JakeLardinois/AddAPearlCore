02d3994865d75337bfaf1ec6dbd24e931d5474bc
Jake Lardinois <jake.lardinois@gmail.com>
2017-11-27 11:47:20 -0600

    Added Production Publish Profile

--- AddAPearl.API/Startup.cs
+++ AddAPearl.API/Startup.cs
@@ -24,8 +24,8 @@ namespace AddAPearl.API
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }

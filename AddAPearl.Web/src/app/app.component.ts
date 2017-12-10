efb9812a7ef0a100c3f80aedf9747be13536ddc4
Jake Lardinois <jake.lardinois@gmail.com>
2017-12-10 09:15:06 -0600

    PageTitle Variable not set

--- AddAPearl.Web/src/app/app.component.ts
+++ AddAPearl.Web/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pearl';
+  public pageTitle: string = `Welcome to Add-A-Pearl!`;
 }
